aa313dfa3691f43ce2a16279e932bad2559407cd
Squallog <lord.subbington@gmail.com>
2026-03-11 18:38:29 +0000

    implemented gubbins functionality

--- assets/scripts/gubbins.js
+++ assets/scripts/gubbins.js
@@ -1,2 +1,8 @@
-let pages = ["/assets/pages/gubbins/terrarium.html", "/assets/pages/gubbins/chilli.html"]
-let gubButton = document.getElementById("gubButton")
+function gubbins () {
+    let pages = ["/assets/pages/gubbins/terrarium.html", "/assets/pages/gubbins/chilli.html"];
+    let gubButton = document.getElementById("gubButton");
+    let selection = Math.round(Math.random()*(pages.length-1));
+    gubButton.href = pages[selection];
+}
+
+gubbins ()
