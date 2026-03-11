81fd45ba728215fea4137ae3d992dc495e23c49c
Squallog <lord.subbington@gmail.com>
2026-03-11 17:29:44 +0000

    Added gubbins pages

--- assets/scripts/gubbins.js
+++ assets/scripts/gubbins.js
@@ -1,2 +1,2 @@
-let pages = ["/assets/pages/gubbins/terrarium.html", "/assets/pages/gubbins/warship.html"]
+let pages = ["/assets/pages/gubbins/terrarium.html", "/assets/pages/gubbins/chilli.html"]
 let gubButton = document.getElementById("gubButton")
